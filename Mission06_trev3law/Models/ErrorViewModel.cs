d76fc0620e75d05c68f7348cad2571b2b00584c0
Trevor_Lawson <trevor3lawson@gmail.com>
2023-02-08 10:36:00 -0700

    Add project files.

---
+++ Mission06_trev3law/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission06_trev3law.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
